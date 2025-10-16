1352307402e4976c5fa32143d0f2a8ffc88e248c
Maulana79 <lanas9066@gmail.com>
2025-10-16 21:50:45 +0800

    Add files via upload

---
+++ prak 6/latihan4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    echo "<title>Halaman PHP pertamaku</title>";
+    ?>
+</head>
+<body>
+    <h1>Script PHP dalam (X)HTML</h1>
+
+    <?php
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    ?>
+
+    <p>Ini paragraph yang dibuat tanpa script PHP</p>
+    <p>Ini paragraph yang dibuat tanpa script PHP</p>
+
+    <?php
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+    ?>
+</body>
+</html>
